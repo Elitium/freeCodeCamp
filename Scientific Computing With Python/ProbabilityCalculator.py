a4b8e2a47c7f3ebb10802f38676afe6fadb9d109
Elitium <70380453+Elitium@users.noreply.github.com>
2024-03-20 20:25:44 +0000

    Update ProbabilityCalculator.py
    Fixed a logic misread, < instead of !=

--- Scientific Computing With Python/ProbabilityCalculator.py
+++ Scientific Computing With Python/ProbabilityCalculator.py
@@ -5,6 +5,7 @@ class Hat:
     def __init__(self, **kwargs):
         self.contents = [item for item, number in kwargs.items() for i in range(number)] # creates list of items
     def draw(self, n):
+        n = min(n, len(self.contents))
         return [self.contents.pop(random.randint(0, len(self.contents) - 1)) for i in range(n)]
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
@@ -14,9 +15,9 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
         ballsDrawn = hatCopy.draw(num_balls_drawn)
         goodPass = True
         for item, number in expected_balls.items():
-            if ballsDrawn.count(item) != number:
+            if ballsDrawn.count(item) < number:
                 goodPass = False
                 break
         if goodPass == True:
-            sucess += 1
+            sucess += 1        
     return sucess/num_experiments
